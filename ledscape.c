85af7289de4619347cd3c73cff65d486e1f0215c
Trammell Hudson <hudson@trmm.net>
1999-12-31 20:10:19 -0500

    output 1 works!

--- ledscape.c
+++ ledscape.c
@@ -29,19 +29,19 @@
  * \todo: Find a way to unify this with the defines in the .p file
  */
 static const uint8_t gpios0[] = {
-	2, 3, 4, 5, 7, 12, 13, 14, 15, 20, 22, 23, 26, 27, 30, 31,
+	27, 22, 10, 9, 8, 26, 11, 30, 31, 5, 3, 20, 4, 2, 14, 7
 };
 
 static const uint8_t gpios1[] = {
-	12, 13, 14, 15, 16, 17, 18, 19, 28, 29,
+	13, 15, 12, 14, 29, 16, 17, 28, 18, 19,
 };
 
 static const uint8_t gpios2[] = {
-	1, 2, 3, 4, 5,
+	2, 5, 22, 23, 14, 12, 10, 8, 6, 3, 4, 1, 24, 25, 17, 16, 15, 13, 11, 9, 7,
 };
 
 static const uint8_t gpios3[] = {
-	16, 19,
+	21, 19, 15, 14, 17, 16
 };
 
 #define ARRAY_COUNT(a) ((sizeof(a) / sizeof(*a)))
