44e8d573b5b8f848e42fcce2a43d60f7852a6ddc
Trammell Hudson <hudson@osresearch.net>
2013-08-04 14:33:01 -0400

    Add warn() and warn_once()

--- util.h
+++ util.h
@@ -10,12 +10,25 @@
 #include <errno.h>
 #include <unistd.h>
 
-#define die(fmt, ...) \
+#define warn(fmt, ...) \
 	do { \
 		fprintf(stderr, "%s:%d: " fmt, \
 			__func__, __LINE__, ## __VA_ARGS__); \
-		exit(EXIT_FAILURE); \
 	} while (0)
 
+#define warn_once(fmt, ...) \
+	do { \
+		static unsigned __warned__; \
+		if (__warned__) \
+			break; \
+		__warned__ = 1; \
+		warn(fmt, ## __VA_ARGS__ ); \
+	} while (0)
+
+#define die(fmt, ...) \
+	do { \
+		warn(fmt, ## __VA_ARGS__ ); \
+		exit(EXIT_FAILURE); \
+	} while(0)
 
 #endif
