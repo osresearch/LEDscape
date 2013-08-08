2a2ea25dbc579a0d4040eb2e42147121221bc705
Trammell Hudson <hudson@trmm.net>
2013-08-08 00:18:33 +0000

    Reliability in the face of flaky hardware. Spin off a separate thread to reopen the teensy devices when they go away. Use a hard coded (soon to be config file) list of teensys.

--- teensy-udp-rx.c
+++ teensy-udp-rx.c
@@ -6,6 +6,10 @@
  *
  * Designed to clock data out to USB teensy3 serial devices, which require
  * bitslicing into the raw form for the OctoWS2811 firmware.
+ *
+ * Since the teensy devices might come and go, this allows them to go into
+ * a disconnected state and will attempt to reconnect when the device port
+ * re-appears.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +23,12 @@
 #include <inttypes.h>
 #include <errno.h>
 #include <string.h>
+#include <pthread.h>
 #include "util.h"
+
+static int port = 9999;
+static unsigned width = 10;
+static unsigned height = 128;
 
 
 /** Prepare one Teensy3 worth of image data.
@@ -95,75 +104,9 @@ udp_socket(
 }
 
 
-static int
-serial_open(
-	const char * const dev
-)
-{
-	const int fd = open(dev, O_RDWR | O_NONBLOCK | O_NOCTTY, 0666);
-	if (fd < 0)
-		return -1;
-
-	// Disable modem control signals
-	struct termios attr;
-	tcgetattr(fd, &attr);
-	attr.c_cflag |= CLOCAL | CREAD;
-	tcsetattr(fd, TCSANOW, &attr);
-
-	return fd;
-}
-
-
-typedef struct
-{
-	unsigned id;
-	unsigned fd;
-	unsigned y_offset;
-	const char * dev;
-	uint8_t bad[256]; // which pixels are to be masked out
-} teensy_strip_t;
-
-// \todo read this from a file at run time
-static teensy_strip_t strips[] = {
-	{ .id = 14401, .y_offset = 0, .bad = { [2] = 1, } },
-	{ .id = 14389, .y_offset = 8, .bad = { [3] = 1, [4] = 1, } },
-	{ .id = 8987, .y_offset = 16, .bad = { [0] = 0xFF, } },
-	{ .id = 8998, .y_offset = 24, .bad = { [1] = 0x80, } },
-};
-
-static const unsigned num_strips = sizeof(strips) / sizeof(*strips);
-	
-
-static teensy_strip_t * strip_find(
-	const char * const dev,
-	const int fd,
-	const unsigned id
-)
-{
-	// Look through our table to find where this one goes
-	for (unsigned j = 0 ; j < num_strips ; j++)
-	{
-		teensy_strip_t * const leds = &strips[j];
-		if (leds->id != id)
-			continue;
-
-		if (leds->dev != NULL)
-		{
-			fprintf(stderr, "FATAL: %s: Duplicate ID %u (%s)\n",
-				dev, id, leds->dev);
-			return NULL;
-		}
-
-		leds->dev = dev;
-		leds->fd = fd;
-		return leds;
-	}
-
-	fprintf(stderr, "FATAL: %s: Unknown ID %u\n", dev, id);
-	return NULL;
-}
-
-
+/** Write all the bytes to a fd, even if there is a brief interruption.
+ * \return number of bytes written or -1 on any fatal error.
+ */
 static ssize_t
 write_all(
 	const int fd,
@@ -194,88 +137,224 @@ write_all(
 }
 		
 
+static int
+serial_open(
+	const char * const dev
+)
+{
+	const int fd = open(dev, O_RDWR | O_NONBLOCK | O_NOCTTY, 0666);
+	if (fd < 0)
+		return -1;
+
+	// Disable modem control signals
+	struct termios attr;
+	tcgetattr(fd, &attr);
+	attr.c_cflag |= CLOCAL | CREAD;
+	tcsetattr(fd, TCSANOW, &attr);
+
+	return fd;
+}
+
+
+typedef struct
+{
+	const char * dev;
+	unsigned id;
+	int fd;
+} teensy_dev_t;
+
+
+typedef struct
+{
+	unsigned id;
+	unsigned y_offset;
+	teensy_dev_t * dev;
+	uint8_t bad[256]; // which pixels are to be masked out
+} teensy_strip_t;
+
+// \todo read this from a file at run time
+static teensy_strip_t strips[] = {
+	{ .id = 14401, .y_offset = 0, .bad = { [2] = 1, } },
+	{ .id = 14389, .y_offset = 8, .bad = { [3] = 1, [4] = 1, } },
+	{ .id = 8987, .y_offset = 16, .bad = { [0] = 0xFF, } },
+	{ .id = 8998, .y_offset = 24, .bad = { [1] = 0x80, } },
+};
+
+
+static teensy_dev_t teensy_devs[] = {
+	{ "/dev/ttyACM0", 0, -1 },
+	{ "/dev/ttyACM1", 0, -1 },
+	{ "/dev/ttyACM2", 0, -1 },
+	{ "/dev/ttyACM3", 0, -1 },
+};
+
+
+static const unsigned num_strips = sizeof(strips) / sizeof(*strips);
+static const unsigned num_devs = sizeof(teensy_devs) / sizeof(*teensy_devs);
+	
+
+static teensy_strip_t *
+strip_find(
+	teensy_dev_t * const dev
+)
+{
+	// Look through our table to find where this one goes
+	for (unsigned j = 0 ; j < num_strips ; j++)
+	{
+		teensy_strip_t * const leds = &strips[j];
+		if (leds->id != dev->id)
+			continue;
+
+		if (leds->dev != NULL)
+		{
+			fprintf(stderr, "FATAL: %s: Duplicate ID %u (%s)\n",
+				dev->dev, dev->id, leds->dev->dev);
+			return NULL;
+		}
+
+		leds->dev = dev;
+		return leds;
+	}
+
+	fprintf(stderr, "FATAL: %s: Unknown ID %u\n", dev->dev, dev->id);
+	return NULL;
+}
+
+
+static void
+strip_close(
+	teensy_strip_t * const strip
+)
+{
+	teensy_dev_t * const dev = strip->dev;
+	if (!dev)
+		return;
+
+	if (dev->fd >= 0)
+		close(dev->fd);
+
+	dev->fd = -1;
+	dev->id = 0;
+	strip->dev = NULL;
+}
+
+
+static int
+teensy_open(
+	teensy_dev_t * const dev
+)
+{
+	dev->fd = serial_open(dev->dev);
+	if (dev->fd < 0)
+	{
+		if (errno == ENOENT)
+			return 0;
+
+		warn("%s: Unable to open serial port: %s\n",
+			dev->dev,
+			strerror(errno)
+		);
+
+		// Something else went wrong
+		goto fail;
+	}
+
+	// Find out the serial number of this teensy
+	ssize_t rc;
+	rc = write_all(dev->fd, "?", 1);
+	if (rc < 0)
+	{
+		warn("%s: write failed: %s\n", dev->dev, strerror(errno));
+		goto fail;
+	}
+
+	// \todo: Read until a newline or a timeout
+	usleep(100000);
+	char response[128];
+	rc = read(dev->fd, response, sizeof(response)-1);
+	if (rc < 0)
+	{
+		warn("%s: read failed: %s\n", dev->dev, strerror(errno));
+		goto fail;
+	}
+
+	response[rc] = '\0';
+	if (0) printf("read: '%s'\n", response);
+
+	unsigned this_width;
+	if (sscanf(response, "%d,%*d,%*d,%d", &this_width, &dev->id) != 2)
+	{
+		warn("%s: Unable to parse response: '%s'\n", dev->dev, response);
+		goto fail;
+	}
+
+	printf("%s: ID %d width %d\n", dev->dev, dev->id, this_width);
+
+	if (dev->id == 0)
+	{
+		warn("%s: dev id == 0.  NOT GOOD\n", dev->dev);
+		goto fail;
+	}
+
+	if (this_width != width)
+	{
+		warn("%s: width %d != expected %d\n", dev->dev, this_width, width);
+		goto fail;
+	}
+
+	teensy_strip_t * const leds = strip_find(dev);
+	if (!leds)
+		goto fail;
+
+	return 1;
+
+fail:
+	dev->id = 0;
+	if (dev->fd >= 0)
+		close(dev->fd);
+	return -1;
+}
+
+
+static void *
+reopen_thread(
+	void * const arg
+)
+{
+	(void) arg;
+
+	while (1)
+	{
+		for (unsigned i = 0 ; i < num_devs ; i++)
+		{
+			teensy_dev_t * const dev = &teensy_devs[i];
+			if (dev->id != 0)
+				continue;
+			teensy_open(dev);
+		}
+
+		sleep(1);
+	}
+
+	return NULL;
+}
+
+
+
+
 int
 main(
 	int argc,
 	char ** argv
 )
 {
-	int port = 9999;
-	unsigned width = 10;
-	unsigned height = 128;
-
-	const unsigned num_devs = argc - 1;
-	if (num_devs != num_strips)
-		die("Must specify %u serial devices\n", num_strips);
-
-	int failed = 0;
-	for (int i = 1 ; i < argc ; i++)
-	{
-		const char * const dev = argv[i];
-		const int fd = serial_open(dev);
-		if (fd < 0)
-		{
-			warn("%s: Unable to open serial port: %s\n",
-				dev,
-				strerror(errno)
-			);
-			failed++;
-			continue;
-		}
-
-		// Find out the serial number of this teensy
-		ssize_t rc;
-		rc = write(fd, "?", 1);
-		if (rc < 0)
-		{
-			warn("%s: write failed: %s\n", dev, strerror(errno));
-			failed++;
-			continue;
-		}
-
-		usleep(100000);
-		char response[128];
-		rc = read(fd, response, sizeof(response)-1);
-		if (rc < 0)
-		{
-			warn("%s: read failed: %s\n", dev, strerror(errno));
-			failed++;
-			continue;
-		}
-
-		response[rc] = '\0';
-		if (0) printf("read: '%s'\n", response);
-
-		unsigned this_width;
-		unsigned this_id;
-		if (sscanf(response, "%d,%*d,%*d,%d", &this_width, &this_id) != 2)
-		{
-			warn("%s: Unable to parse response: '%s'\n", dev, response);
-			failed++;
-			continue;
-		}
-
-		printf("%s: ID %d width %d\n", dev, this_id, this_width);
-		if (this_width != width)
-		{
-			warn("%s: width %d != expected %d\n", dev, this_width, width);
-			failed++;
-			continue;
-		}
-
-		teensy_strip_t * const leds = strip_find(dev, fd, this_id);
-		if (!leds)
-			failed++;
-	}
-
-	if (failed)
-		die("FATAL CONFIGURATION ERROR\n");
-
-	printf("%d serial ports\n", num_devs);
-
 	const int sock = udp_socket(port);
 	if (sock < 0)
 		die("socket port %d failed: %s\n", port, strerror(errno));
+
+	pthread_t reopen_tid;
+	if (pthread_create(&reopen_tid, NULL, reopen_thread, NULL) < 0)
+		die("pthread create failed: %s\n", strerror(errno));
 
 	const size_t image_size = width * height * 3;
 	const size_t slice_size = width * 8 * 3 + 3;
@@ -327,7 +406,12 @@ main(
 		// for each teensy.
 		for (unsigned i = 0 ; i < num_strips ; i++)
 		{
-			const teensy_strip_t * const strip = &strips[i];
+			teensy_strip_t * const strip = &strips[i];
+			teensy_dev_t * const dev = strip->dev;
+
+			// If it hasn't been opened yet, go onto the next one
+			if (!dev)
+				continue;
 
 			bitslice(
 				slice + 3,
@@ -338,21 +422,25 @@ main(
 			);
 
 			const ssize_t rc
-				= write_all(strip->fd, slice, slice_size);
+				= write_all(dev->fd, slice, slice_size);
+
+			if ((size_t) rc == slice_size)
+				continue;
 
 			if (rc < 0)
-				die("%s: write failed: %s\n",
+				warn("%s: write failed: %s\n",
 					strip->dev,
 					strerror(errno)
 				);
-
-			if ((size_t) rc != slice_size)
-				die("%s: short write %zu != %zu: %s\n",
+			else
+				warn("%s: short write %zu != %zu: %s\n",
 					strip->dev,
 					rc,
 					slice_size,
 					strerror(errno)
 				);
+
+			strip_close(strip);
 		}
 	}
 
