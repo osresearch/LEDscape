6dbec3c10b04bd59cad94f5fe19a46a7cfb461bf
root <root@ledscape.(none)>
2014-05-07 01:05:43 +0000

    getoptified stuff

--- src/net/matrix-udp-rx.c
+++ src/net/matrix-udp-rx.c
@@ -21,9 +21,11 @@
 #include <errno.h>
 #include <string.h>
 #include <pthread.h>
+#include <getopt.h>
 #include "util.h"
 #include "ledscape.h"
 
+static int verbose;
 
 static int
 udp_socket(
@@ -45,6 +47,26 @@ udp_socket(
 	return sock;
 }
 
+static struct option long_options[] =
+{
+	/* These options set a flag. */
+	{"verbose", no_argument,       0, 1},
+	{"port",    required_argument, 0, 'p'},
+	{"width",   required_argument, 0, 'W'},
+	{"height",  required_argument, 0, 'H'},
+	{"config",  required_argument, 0, 'c'},
+	{"timeout", required_argument, 0, 't'},
+	{"message", required_argument, 0, 'm'},
+	{0, 0, 0, 0}
+};
+
+
+static void usage(void)
+{
+	fprintf(stderr, "usage not yet written\n");
+	exit(EXIT_FAILURE);
+}
+
 
 int
 main(
@@ -52,17 +74,61 @@ main(
 	char ** argv
 )
 {
-	const int port = 9999;
+	/* getopt_long stores the option index here. */
+	int option_index = 0;
+	int port = 9999;
+	const char * config_file = NULL;
+	const char * startup_message = "";
+	int timeout = 60;
+	unsigned width = 512;
+	unsigned height = 64;
+
+	while (1)
+	{
+		const int c = getopt_long(
+			argc,
+			argv,
+			"vp:c:t:W:H:m:",
+			long_options,
+			&option_index
+		);
+
+		if (c == -1)
+			break;
+		switch (c)
+		{
+		case 'v':
+			verbose++;
+			break;
+		case 'c':
+			config_file = optarg;
+			break;
+		case 't':
+			timeout = atoi(optarg);
+			break;
+		case 'W':
+			width = atoi(optarg);
+			break;
+		case 'H':
+			height = atoi(optarg);
+			break;
+		case 'm':
+			startup_message = optarg;
+			break;
+		default:
+			usage();
+			return -1;
+		}
+	}
+
 	const int sock = udp_socket(port);
 	if (sock < 0)
 		die("socket port %d failed: %s\n", port, strerror(errno));
 
-	const unsigned width = 512;
-	const unsigned height = 64;
 	const size_t image_size = width * height * 3;
 
 	// largest possible UDP packet
-	uint8_t buf[width*height*4];
+	uint8_t *buf = calloc(width*height,4);
 #if 0
 	if (sizeof(buf) < image_size + 1)
 		die("%u x %u too large for UDP\n", width, height);
@@ -71,9 +137,9 @@ main(
 	fprintf(stderr, "%u x %u, UDP port %u\n", width, height, port);
 
 	ledscape_config_t * config = &ledscape_matrix_default;
-	if (argc > 1)
+	if (config_file)
 	{
-		config = ledscape_config(argv[1]);
+		config = ledscape_config(config_file);
 		if (!config)
 			return EXIT_FAILURE;
 	}
@@ -94,6 +160,8 @@ main(
 	unsigned frames = 0;
 
 	uint32_t * const fb = calloc(width*height,4);
+	ledscape_printf(fb, width, 0xFF0000, "%s", startup_message);
+	ledscape_printf(fb+16*width, width, 0x00FF00, "%dx%d UDP port %d", width, height, port);
 	ledscape_draw(leds, fb);
 
 	while (1)
